8e39833361a90b92fca530773eff7c3995b11a0e
martinviretti <martinviretti@gmail.com>
2022-07-19 19:30:04 -0300

    1

---
+++ Practias Extra/app.js
@@ -0,0 +1,70 @@
+// 1. Ejercicio 1: imprima la suma de todos los números impares entre 1 y 100
+
+// let acu = 0
+
+// for(i = 0 ; i <= 100 ; i++){
+//  if((i % 2) != 0 ){
+//    acu += i
+// }
+// }
+// alert( " La suma de todos los numeros impares entre 1 y 100 es " + " " + acu)
+
+
+
+// Ejercicio 2 Imprime el número y la suma de todos los múltiplos de 7 entre 1-100
+// let sum = 0;
+// let arr01 = [];
+
+// for(let i=1;i <=100;i++){
+//     if(i%7 == 0){
+//         arr01.push(i);
+//         sum=sum+i;
+//     }
+// }
+//              alert('Todos los múltiplos de 7 entre 1-100 son:' + arr01 + '<br/>');
+//              alert('El número de múltiplos de los 7 entre 1-100 es:' + arr01.length + '<br/>');
+//              alert('La suma de todos los múltiplos de 7 entre 1-100 es:' + sum + '<br/>');
+
+// El número de narcisos se refiere a un número de 3 dígitos y la suma de los dígitos a la potencia de 3 es igual a sí mismo. (Por ejemplo: 1 ^ 3 + 5 ^ 3 + 3 ^ 3 = 153), imprima todos los números de narcisos.
+
+// var num = [];
+// for(var i=100;i<=1000;i++){
+//     var ge=i%10;
+//     var shi=parseInt(i/10)%10;
+//     var bai=parseInt(i/100);
+//     if(ge**3+shi**3+bai**3==i){
+//         num.push(i);
+//     }
+// }
+// document.write(num);
+
+
+// Reciba un número ingresado por el usuario en la página y determine si el número es un número primo. Número primo: Un número que solo puede ser divisible por 1 y él mismo. 1 no es un número primo ni un número compuesto. Un número primo debe ser un número natural mayor que 1.
+
+// let numb = prompt("Ingrese un numero: ")
+
+//  if( numb ==! ( numb % 2) == 0){
+//     alert("el numero es primo")
+  
+// }else{
+//     alert("el numero no es primo")
+// }
+
+// falto el ciclo
+
+
+// 5. Imprime la tabla de multiplicar del 99
+
+// table = []
+// for(let i = 1 ; i <= 10 ; i++){
+//     let tablaDelNueve =  99 * i
+//     table.push(tablaDelNueve)
+
+// }
+// alert(table)
+
+// dudoso
+
+// 6. Imprima todos los números primos entre 1 y 100
+
+
