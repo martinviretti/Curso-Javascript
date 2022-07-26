652f7d55be238f9a20a55833c010e6eee653ce68
martinviretti <martinviretti@gmail.com>
2022-07-25 22:26:08 -0300

    1

--- Desafrio(1)-simuladorInteractivo/Ejemplo-1/app.js
+++ Desafrio(1)-simuladorInteractivo/Ejemplo-1/app.js
@@ -1,9 +1,23 @@
-let MontoDeterminado = parseInt(prompt("Ingese el total de compra :"))
+let Producto1 = parseInt(prompt(" Ingrese el precio del producto 1 :"))
+let Producto2 = parseInt(prompt(" Ingrese el precio del producto 2 :"))
+let Producto3 = parseInt(prompt(" Ingrese el precio del producto 3 :"))
+let Producto4 = parseInt(prompt(" Ingrese el precio del producto 4 :"))
 
 
-while(isNaN(MontoDeterminado) || MontoDeterminado <= 0 ||MontoDeterminado === " " ){
-  MontoDeterminado = ParseInt(prompt(" No ingreso un Monto valido, Ingrse el total de compra:"))
+while(isNaN(Producto1 ) || isNaN(Producto2 ) || isNaN(Producto3 ) || isNaN(Producto4 )  || "") {
+    alert("Error alguno de los ingresos no fue valido , por favor ingrese nuevamente")
+     Producto1 = parseInt(prompt(" Ingrese el precio del producto 1 :"))
+     Producto2 = parseInt(prompt(" Ingrese el precio del producto 2 :"))
+     Producto3 = parseInt(prompt(" Ingrese el precio del producto 3 :"))
+     Producto4 = parseInt(prompt(" Ingrese el precio del producto 4 :"))
+
 }
+
+
+let MontoDeterminado = Producto1 + Producto2 + Producto3 + Producto4
+
+
+
 
 let CantidadDeCuotas = parseInt(prompt("Cantidad de cuotas :"))
 
@@ -16,18 +30,18 @@ while(isNaN(CantidadDeCuotas) || CantidadDeCuotas <= 0){
 function CalcularCuotas (MontoDeterminado,CantidadDeCuotas){
     if (CantidadDeCuotas > 0 && CantidadDeCuotas<= 5)
     {
-       let total = ( " su cuota seria de :" + ( (MontoDeterminado / CantidadDeCuotas) * 1.25))
+       let total = ( " Su cuota tiene un 25% de interes y  el monto es :" + ( (MontoDeterminado / CantidadDeCuotas) * 1.25))
        return total
     }
     else if(CantidadDeCuotas > 5 && CantidadDeCuotas < 12)
     {
-       let total = ( " su cuota seria de :" +  ( (MontoDeterminado / CantidadDeCuotas) * 1.40))
+       let total = ( " Su cuota tiene un 40% de interes y  el monto es :" +  ( (MontoDeterminado / CantidadDeCuotas) * 1.40))
        return total
 
     }
     else ( CantidadDeCuotas >= 12)
     {
-        let total = ( " su cuota seria de :" +  ( (MontoDeterminado / CantidadDeCuotas) * 1.60))
+        let total = ( " Su cuota tiene un 60% de interes y  el monto es :"+ " " + " " +  ( (MontoDeterminado / CantidadDeCuotas) * 1.60))
          return total
     }
 
--- Desafrio(1)-simuladorInteractivo/Ejemplo-2/app.js
+++ Desafrio(1)-simuladorInteractivo/Ejemplo-2/app.js
@@ -5,60 +5,59 @@ while(MontoApagar == " " || isNaN(MontoApagar)){
     MontoApagar = parseInt(prompt("Error ,Ingrese el monto a abonar: "))
 }
 
-
-
 let descuento = parseInt(prompt("Ingrse su descuento:" + " 25% , 35% o 50% " ))
+
+while(descuento ==! 25 || 35 || 50){
+  descuento = parseInt(prompt("ingrese un descuento valido"))
+  
+}
 
 let impuestos = 1.21
 
- if(descuento === 25){
-    descuento = 0.75
+function validarDescuento (descueno){
 
-}else if(descuento === 35){
-    descuento = 0.65
-
-}else if(descuento === 50){
-    descuento = 0.50
-
-}else  while(descuento !== 25 || descuento !== 35 || descuento !== 50 || descuento === " " || isNaN(descuento)){
-        descuento = parseInt(prompt("ingrse un descueno valido : "))
-        
-        if(isNaN(descuento)){  
-            
-            if(descuento == 25){
-            descuento = 0.75
-            continue
-        }else if(descuento == 35){ 
-            descuento = 0.65
-            continue
-            ;
-        }else(descuento == 50) ;{
-            
-            descuento = 0.50
-            continue
-     
-    }
-
-
-
-      
-
-
-        }
-      
+    if(descuento === 25){
+        descuento = 0.75
     
+    }else if(descuento === 35){
+        descuento = 0.65
     
-
-
-  alert( CalcularResultadoTotal(MontoApagar,descuento,impuestos))
-
-
-function CalcularResultadoTotal (MontoApagar,descuento,impuestos){
-   
-  let  montFinal = (MontoApagar * descuento ) * impuestos 
-
-    return montFinal 
+    }else (descuento === 50)
+        descuento = 0.50
+       
 }
 
+// validarDescuento(descuento)
+ 
 
-// cuando pongo un descuento invalido me entra en el bucle while y aunque ponga un descuento bien no sale 
+
+//   while(descuento !== 25 || descuento !== 35 || descuento !== 50 || descuento === " " || isNaN(descuento)){
+//         descuento = parseInt(prompt("ingrse un descueno valido : "))
+        
+//         if(isNaN(descuento)){  
+            
+//             if(descuento == 25){
+//             descuento = 0.75
+//             continue
+//         }else if(descuento == 35){ 
+//             descuento = 0.65
+//             continue
+//             ;
+//         }else(descuento == 50) ;{
+            
+//             descuento = 0.50
+//             continue
+     
+//     }
+//         }
+      
+//   alert( CalcularResultadoTotal(MontoApagar,descuento,impuestos))
+
+// function CalcularResultadoTotal (MontoApagar,descuento,impuestos){
+   
+//   let  montFinal = (MontoApagar * descuento ) * impuestos 
+
+//     return montFinal 
+// }
+
+// // cuando pongo un descuento invalido me entra en el bucle while y aunque ponga un descuento bien no sale 
