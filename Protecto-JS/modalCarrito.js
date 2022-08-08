0c611284fd55d3249a0dbe10b41f8cb5e1812ea3
martinviretti <martinviretti@gmail.com>
2022-08-08 17:39:11 -0300

    1

--- Protecto-JS/modalCarrito.js
+++ Protecto-JS/modalCarrito.js
@@ -1,22 +1,123 @@
-const contenedorModal = document.getElementsByClassName('modal-contenedor')[0]
-const botonAbrir = document.getElementById('boton-carrito')
-const botonCerrar = document.getElementById('carritoCerrar')
-const modalCarrito = document.getElementsByClassName('modal-carrito')[0]
+// const contenedorModal = document.getElementsByClassName('modal-contenedor')[0]
+// const botonAbrir = document.getElementById('boton-carrito')
+// const botonCerrar = document.getElementById('carritoCerrar')
+// const modalCarrito = document.getElementsByClassName('modal-carrito')[0]
 
-botonAbrir.addEventListener('click', ()=>{
-    contenedorModal.classList.toggle('modal-active')
-})
-botonCerrar.addEventListener('click', ()=>{
-    contenedorModal.classList.toggle('modal-active')
-})
+// botonAbrir.addEventListener('click', ()=>{
+//     contenedorModal.classList.toggle('modal-active')
+// })
+// botonCerrar.addEventListener('click', ()=>{
+//     contenedorModal.classList.toggle('modal-active')
+// })
 
-contenedorModal.addEventListener('click', ()=>{
-    // contenedorModal.classList.toggle('modal-active')
-    botonCerrar.click()
-})
+// contenedorModal.addEventListener('click', ()=>{
+//     // contenedorModal.classList.toggle('modal-active')
+//     botonCerrar.click()
+// })
 
-modalCarrito.addEventListener('click', (event)=>{
-    // console.log(event)
-    event.stopPropagation()
-})
+// modalCarrito.addEventListener('click', (event)=>{
+//     // console.log(event)
+//     event.stopPropagation()
+// })
 
+
+
+
+
+// ,
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 5,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 6,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 7,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 8,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 9,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 10,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 11,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 12,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 13,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 14,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 15,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 16,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 17,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 18,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     {
+//         "imgUrl": "images/bober/Bobber VT600.jpg",
+//         "id": 19,
+//         "nombre": "bober1",
+//         "precio": 300
+//     },
+//     { 
+//         "imgUrl":"images/bober/Bobber VT600.jpg",
+//         "id": 20,
+//         "nombre":"bober1",
+//         "precio": 300
+//         }
